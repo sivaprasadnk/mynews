0200c0ce85fd3f975c73789904e3f9389f7b3146
sivaprasadnk <sivaprasadnk123@gmail.com>
2024-07-18 18:16:18 +0530

    adding loading shimmer

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:news_app/core/utils/common_colors.dart';
 import 'package:news_app/core/utils/common_strings.dart';
 import 'package:news_app/presentation/provider/firebase_provider.dart';
 import 'package:news_app/presentation/provider/news_provider.dart';
+import 'package:news_app/presentation/widgets/loading_shimmer.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -57,26 +58,32 @@ class _HomeScreenState extends State<HomeScreen> {
           const SizedBox(width: 16),
         ],
       ),
-      body: const Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.only(
-              top: 16,
-              left: 16,
-            ),
-            child: Text(
-              'Top Headlines',
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
+      body: RefreshIndicator(
+        onRefresh: () async {
+          var code = context.read<FirebaseProvider>().countryCode;
+          context.read<NewsProvider>().fetchNews(code);
+        },
+        child: const Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: 16,
+                left: 16,
+              ),
+              child: Text(
+                'Top Headlines',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: NewsList(),
-          ),
-        ],
+            Expanded(
+              child: NewsList(),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -88,88 +95,165 @@ class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return RefreshIndicator(
-      onRefresh: () async {
-        var code = context.read<FirebaseProvider>().countryCode;
-        context.read<NewsProvider>().fetchNews(code);
-      },
-      child: Consumer<NewsProvider>(
-        builder: (context, provider, child) {
-          if (provider.isLoading) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (provider.message != null) {
-            return Center(child: Text(provider.message!));
-          } else {
-            return ListView.builder(
-              padding: const EdgeInsets.all(16),
-              itemCount: provider.news?.length ?? 0,
-              itemBuilder: (context, index) {
-                final newsItem = provider.news![index];
-                return newsItem.title!.isNotEmpty &&
-                        newsItem.description!.isNotEmpty
-                    ? Container(
-                        margin: const EdgeInsets.only(bottom: 16),
-                        padding: const EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: kWhiteColor,
-                        ),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            SizedBox(
-                              width: width * 0.55,
-                              child: Column(
-                                mainAxisSize: MainAxisSize.min,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  SizedBox(
-                                    width: width * 0.6,
-                                    child: Text(
-                                      newsItem.title ?? "",
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.w700,
-                                      ),
-                                      overflow: TextOverflow.ellipsis,
+    return Consumer<NewsProvider>(
+      builder: (context, provider, child) {
+        if (provider.isLoading) {
+          return LoadingList(width: width);
+          // return const Center(child: CircularProgressIndicator());
+        } else if (provider.message != null) {
+          return Center(child: Text(provider.message!));
+        } else {
+          return ListView.builder(
+            padding: const EdgeInsets.all(16),
+            itemCount: provider.news?.length ?? 0,
+            itemBuilder: (context, index) {
+              final newsItem = provider.news![index];
+              return newsItem.title!.isNotEmpty &&
+                      newsItem.description!.isNotEmpty &&
+                      newsItem.title != "[Removed]"
+                  ? Container(
+                      margin: const EdgeInsets.only(bottom: 16),
+                      padding: const EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: kWhiteColor,
+                      ),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          SizedBox(
+                            width: width * 0.55,
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                SizedBox(
+                                  width: width * 0.6,
+                                  child: Text(
+                                    newsItem.title ?? "",
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.w700,
                                     ),
+                                    overflow: TextOverflow.ellipsis,
                                   ),
-                                  const SizedBox(height: 15),
-                                  SizedBox(
-                                    width: width * 0.55,
-                                    child: Text(
-                                      newsItem.description ?? "",
-                                      style: const TextStyle(),
-                                      maxLines: 3,
-                                      overflow: TextOverflow.ellipsis,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            ),
-                            const Spacer(),
-                            if (newsItem.imageUrl != null)
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(10),
-                                child: Image.network(
-                                  newsItem.imageUrl!,
-                                  height: 109,
-                                  width: 109,
-                                  fit: BoxFit.cover,
-                                  errorBuilder: (context, error, stackTrace) {
-                                    return const Icon(Icons.error);
-                                  },
                                 ),
-                              )
-                          ],
-                        ),
-                      )
-                    : const SizedBox.shrink();
-              },
-            );
-          }
-        },
-      ),
+                                const SizedBox(height: 15),
+                                SizedBox(
+                                  width: width * 0.55,
+                                  child: Text(
+                                    newsItem.description ?? "",
+                                    style: const TextStyle(),
+                                    maxLines: 3,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                          const Spacer(),
+                          if (newsItem.imageUrl != null)
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Image.network(
+                                newsItem.imageUrl!,
+                                height: 109,
+                                width: 109,
+                                fit: BoxFit.cover,
+                                errorBuilder: (context, error, stackTrace) {
+                                  return const Icon(Icons.error);
+                                },
+                              ),
+                            )
+                          else
+                            Container(
+                              height: 109,
+                              width: 109,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: kBlueColor,
+                                ),
+                              ),
+                              child: const Center(
+                                child: Text(" No Image"),
+                              ),
+                            )
+                        ],
+                      ),
+                    )
+                  : const SizedBox.shrink();
+            },
+          );
+        }
+      },
+    );
+  }
+}
+
+class LoadingList extends StatelessWidget {
+  const LoadingList({
+    super.key,
+    required this.width,
+  });
+
+  final double width;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemCount: 5,
+      itemBuilder: (context, index) {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 16),
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: kWhiteColor,
+          ),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                width: width * 0.55,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: width * 0.6,
+                      child: LoadingShimmer.rectangle(
+                        height: 20,
+                        width: width * .55,
+                        context: context,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      width: width * 0.55,
+                      child: LoadingShimmer.rectangle(
+                        height: 20,
+                        width: width * .55,
+                        context: context,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const Spacer(),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: LoadingShimmer.rectangle(
+                  height: 109,
+                  width: 109,
+                  context: context,
+                ),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
